7d5c9ab6748392d27b5075d1a0e2d878f3bc2946
Mohammad Ali <mohalikhan@hotmail.com>
2019-12-02 11:14:33 -0500

    Move data to Firebase, removed local data file, introduced HOC through spinner. Upload data to Firebase (onetime) through helper function (comment code)

--- src/App.js
+++ src/App.js
@@ -11,9 +11,14 @@ import CheckoutPage from './pages/checkout/checkout.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 import Header from './components/header/header.component';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import {
+    auth,
+    createUserProfileDocument,
+    //addCollectionAndDocuments
+} from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
+//import { selectCollectionsForPreview } from './redux/shop/shop.selectors';
 
 class App extends React.Component {
 
@@ -21,6 +26,7 @@ class App extends React.Component {
 
   componentDidMount() {
 
+    //const { setCurrentUser, collectionArray } = this.props;
     const { setCurrentUser } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
@@ -35,6 +41,7 @@ class App extends React.Component {
         });
       }
       setCurrentUser(userAuth);
+      //addCollectionAndDocuments('collections', collectionArray);
     });
   }
 
@@ -68,7 +75,8 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
+  //collectionArray: selectCollectionsForPreview
 });
 
 const mapDispatchToProps = dispatch => ({
