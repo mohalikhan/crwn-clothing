3a8464595d116a4d1e90513be63f5ccb2e5f7596
Mohammad Ali <mohalikhan@hotmail.com>
2019-12-17 20:39:12 -0500

    Add node server to request payments for stripe and move UI to client folder.

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
-const StripeCheckoutButton = ({price}) => {
+const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publilshAbleKey = 'pk_test_1TJoAjQXFPotGRTy3eui9NsU00WUNkeill';
 
     const onToken = token => {
-        console.log(token);
-        alert('Payment Successful');
+       axios({
+           url: 'payment',
+           method: 'post',
+           data: {
+               amount: priceForStripe,
+               token
+           }
+       }).then(response => {
+            alert('successful');
+       }).catch(error => {
+            console.log('Payment error:' + JSON.parse(error));
+            alert('There was an issue with your payment. Please sure you use the provided credit card.');
+       });
     }
 
     return (
-        <StripeCheckout 
+        <StripeCheckout
             label='Pay Now'
-            name ='CRWN Clothing Ltd.'
+            name='CRWN Clothing Ltd.'
             billingAddress
             shippingAddress
             image='https://svgshare.com/i/Cuz.svg'
